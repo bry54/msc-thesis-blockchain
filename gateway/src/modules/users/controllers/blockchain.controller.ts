a852726c12212dde5ed58692ffffde04d7ff4815
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2024-05-19 11:49:28 +0300

    Added templates for blockchain controller and service.
    Signed-off-by: Brian Paidamoyo Sithole <brianacyth@icloud.com>

---
+++ gateway/src/modules/users/controllers/blockchain.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { BlockchainService } from '../services/blockchain.service';
+
+const resource: string = 'user';
+
+@ApiTags(`Blockchain ${resource} queries`)
+@Controller(`blockchain/${resource}`)
+export class BlockchainController {
+  constructor(public service: BlockchainService) {}
+
+  @Get('')
+  @ApiOperation({ summary: `Get all current world state for ${resource} records as they are on the blockchain` })
+  async findMany() {
+    return this.service.findMany();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: `Get the current world state for a ${resource} record` })
+  @ApiParam({ name: 'id', description: `The ID of the ${resource} to query on the blockchain`, type: String })
+  async findOne(@Param() id: string) {
+    return this.service.findOne(id);
+  }
+
+  @Get(':id/history')
+  @ApiOperation({ summary: `Get the entire history a ${resource} record` })
+  @ApiParam({ name: 'id', description: `The ID of the ${resource} whose history will be queried on blockchain`, type: String })
+  async history(@Param('id') id: string) {
+    return this.service.history(id);
+  }
+}
--- gateway/src/modules/users/users.module.ts
+++ gateway/src/modules/users/users.module.ts
@@ -3,11 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { BlockchainService } from './services/blockchain.service';
+import { BlockchainController } from './controllers/blockchain.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, BlockchainController],
+  providers: [UsersService, BlockchainService],
   exports: [UsersService],
 })
 export class UsersModule {}
