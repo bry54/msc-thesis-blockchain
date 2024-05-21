717fbadc66a0d83fa6d8972841e54a2dcdb899de
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2024-05-21 15:10:29 +0300

    Added table to users view
    Signed-off-by: Brian Paidamoyo Sithole <brianacyth@icloud.com>

--- frontend/src/app/dashboard/users/page.tsx
+++ frontend/src/app/dashboard/users/page.tsx
@@ -1,4 +1,71 @@
+'use client'
+
+import {Table, TableColumnsType, TableProps} from "antd";
+
 export default function UsersPage() {
+    interface DataType {
+        key: React.Key;
+        name: string;
+        age: number;
+        address: string;
+    }
+
+    const columns: TableColumnsType<DataType> = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            showSorterTooltip: { target: 'full-header' },
+            filters: [ ],
+            // specify the condition of filtering result
+            // here is that finding the name started with `value`
+            onFilter: (value, record) => record.name.indexOf(value as string) === 0,
+            sorter: (a, b) => a.name.length - b.name.length,
+            sortDirections: ['descend'],
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            sorter: (a, b) => a.age - b.age,
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            filters: [],
+            onFilter: (value, record) => record.address.indexOf(value as string) === 0,
+        },
+    ];
+
+    const data = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sydney No. 1 Lake Park',
+        },
+        {
+            key: '4',
+            name: 'Jim Red',
+            age: 32,
+            address: 'London No. 2 Lake Park',
+        },
+    ];
+
+    const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+    };
+
     return (
         <div className='text-neutral-950'>
             <header className="bg-white shadow">
@@ -8,7 +75,12 @@ export default function UsersPage() {
             </header>
             <main>
                 <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                    Users Content
+                    <Table
+                        columns={columns}
+                        dataSource={data}
+                        onChange={onChange}
+                        showSorterTooltip={{ target: 'sorter-icon' }}
+                    />
                 </div>
             </main>
         </div>
