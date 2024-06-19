817e6d4fd4e15130f67271f53032889e53aa6cd7
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2024-06-19 14:29:21 +0300

    Fix on transportation-details.js

--- agro-blockchain/src/screens/update-partials/transportation-details.js
+++ agro-blockchain/src/screens/update-partials/transportation-details.js
@@ -1,8 +1,9 @@
-import {Button, Divider, Input, ListItem, Overlay, Text} from "@rneui/themed";
+import {Button, Divider, Icon, Input, ListItem, Overlay, Text} from "@rneui/themed";
 import React, {useEffect, useState} from "react";
-import {View} from "react-native";
+import {View, StyleSheet} from "react-native";
 import axios from "axios";
 import {API_HOST} from "../../store/constants";
+import { Dropdown } from 'react-native-element-dropdown';
 
 const EditBtn = ({ onPress }) =>{
     return (
@@ -36,29 +37,101 @@ const TransportationDetail = ({ data }) => {
     )
 }
 
+const LocationSelect = ({ options, location, onChangeFn }) => {
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    let opts = options
+
+    if (location === 'destination'){
+        opts = options.filter(option => option.id !== value);
+    }
+
+    return (
+        <Dropdown
+            style={[styles.dropdown, isFocus && { borderColor: 'transparent', borderWidth: 0 }]}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={ opts }
+            maxHeight={300}
+            labelField="name"
+            valueField="id"
+            placeholder={!isFocus ? `Select ${location}` : '...'}
+            searchPlaceholder="Search..."
+            value={value}
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setIsFocus(false)}
+            onChange={item => {
+                setValue(`${item.id}`);
+                setIsFocus(false);
+                const value = {
+                    name: `${item.name}, ${item.type}`,
+                    address: item.location,
+                }
+                // Create a new object based on the updates needed
+                const updates = Object.keys(value).reduce((acc, v) => {
+                    acc[v] = value[v];
+                    return acc;
+                }, {});
+
+                onChangeFn(location, 'obj', updates);
+            }}
+            renderLeftIcon={() => (
+                <Icon
+                    style={styles.icon}
+                    color={isFocus ? 'blue' : 'black'}
+                    name="place"
+                    size={20}
+                />
+            )}
+        />
+    )
+}
+
 export const UpdateTransportationDetails = ({ product }) => {
     const [editableElement, setEditableElement] = useState(null);
     const [records, setRecords] = useState([]);
     const [selectedRec, setSelectedRec] = useState(null);
     const [visible, setVisible] = useState(false);
+    const [locations, setLocations] = useState([]);
 
     const queryRecords = async () =>{
         const response = await axios.get(`${API_HOST}/transportation-details/${product.id}`)
         const data = response.data
 
+        const locResponse = await axios.get(`${API_HOST}/stakeholder`)
+        const locations = locResponse.data.data
+
         setRecords(data)
+        setLocations(locations)
     }
 
     const updateRecDetails = (mainProp, prop, value) => {
-        setSelectedRec((prev) => (
-            {
-                ...prev,
-                [mainProp]: {
-                    ...prev[mainProp],
-                    [prop]: value
+        if (prop === 'obj'){
+            // Update the state with all changes at once
+            setSelectedRec(prevState => {
+                const currentMainPropState = prevState[mainProp] || {};
+                return {
+                    ...prevState,
+                    [mainProp]: {
+                        ...currentMainPropState,
+                        ...value
+                    }
                 }
-            }
-        ));
+            });
+        } else {
+            setSelectedRec((prev) => (
+                {
+                    ...prev,
+                    [mainProp]: {
+                        ...prev[mainProp],
+                        [prop]: value
+                    }
+                }
+            ));
+        }
     }
 
     const patchRec = async () => {
@@ -97,21 +170,34 @@ export const UpdateTransportationDetails = ({ product }) => {
                     {
                         !selectedRec?.id && (
                             <>
-                                <Text>From: </Text>
-                                <Input
-                                    placeholder='Enter departure notes'
-                                    value={selectedRec?.departure.notes || ''}
-                                    onChangeText={value => updateRecDetails('departure', 'notes', value)}
-                                />
-
+                                <Text h4 style={{marginVertical: 10, fontWeight: 'light'}}> Provide transportation data</Text>
                                 <Divider/>
+                                <View style={{marginVertical: 10}}>
+                                    <LocationSelect
+                                        location='departure'
+                                        options={locations}
+                                        onChangeFn={updateRecDetails}
+                                    />
 
-                                <Text>From: </Text>
-                                <Input
-                                    placeholder='Enter destination notes'
-                                    value={selectedRec?.destination.notes || ''}
-                                    onChangeText={value => updateRecDetails('destination', 'notes', value)}
-                                />
+                                    <Input
+                                        placeholder='Enter departure notes'
+                                        value={selectedRec?.departure.notes || ''}
+                                        onChangeText={value => updateRecDetails('departure', 'notes', value)}
+                                    />
+                                </View>
+
+                                {selectedRec?.departure.name && <View style={{marginVertical: 10}}>
+                                    <LocationSelect
+                                        location='destination'
+                                        options={locations}
+                                        onChangeFn={updateRecDetails}
+                                    />
+                                    <Input
+                                        placeholder='Enter destination notes'
+                                        value={selectedRec?.destination.notes || ''}
+                                        onChangeText={value => updateRecDetails('destination', 'notes', value)}
+                                    />
+                                </View>}
                             </>
                         )
                     }
@@ -208,3 +294,43 @@ export const UpdateTransportationDetails = ({ product }) => {
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        padding: 16,
+    },
+    dropdown: {
+        height: 50,
+        borderColor: 'gray',
+        borderBottomWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+    },
+    icon: {
+        marginRight: 5,
+    },
+    label: {
+        position: 'absolute',
+        backgroundColor: 'white',
+        left: 22,
+        top: 8,
+        zIndex: 999,
+        paddingHorizontal: 8,
+        fontSize: 14,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+});
